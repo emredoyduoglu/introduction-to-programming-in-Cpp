08811368b30c1a0c2f35a7104bfa6c2d0e987e80
Emrah Emre Doyduoğlu <93899430+emredoyduoglu@users.noreply.github.com>
2021-11-10 10:29:57 +0300

    Update counter.cpp

--- module02-data-types-and-expression/counter.cpp
+++ module02-data-types-and-expression/counter.cpp
@@ -5,12 +5,42 @@ Write a program that asks the user to enter a number of quarters, dimes, nickels
 Your program should interact with the user exactly as it shows in the following example:
 
 Please enter the number of coins:<br>
-# of quarters: 20<br>
-# of dimes: 4<br>
-# of nickels: 13<br>
-# of pennies: 17<br>
-The total is 6 dollars and 22 cents<br>
+# of quarters: 20
+# of dimes: 4
+# of nickels: 13
+# of pennies: 17
+The total is 6 dollars and 22 cents
 
 File Name 
 
-counter.cpp*/
+counter.cpp
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int quarters,dimes,nickels,pennies;
+    int money;
+	int dollars;
+	int cents;
+	
+	cout<<"Please enter the number of coins: "<<endl;
+	cout<<"# of quarters: ";
+	cin>>quarters;
+	cout<<"# of dimes: ";
+	cin>>dimes;
+	cout<<"# of nickels: ";
+	cin>>nickels;
+	cout<<"# of pennies: ";
+	cin>>pennies;
+	
+	money = pennies + (5*nickels)+ (10*dimes)+(25*quarters);
+	dollars = money /100;
+	cents = money%100;
+	
+	cout<<"The total is "<<dollars<<" dollars and "<<cents<<" cents"<<endl;
+    return 0;
+}
